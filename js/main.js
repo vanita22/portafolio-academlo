4e64ff3649d963cd0ec1002e1c015442b717e266
vanita22 <inesvanessapcm@gmail.com>
2021-08-18 03:08:31 -0400

    my first commit

--- js/main.js
+++ js/main.js
@@ -11,4 +11,28 @@ $(document).ready(function () {
         $('#btn-click').show(200);  
         $('#submenu').hide();      
     });
+
+    tamano='';
+    i=1;
+    ConvertirTexto();
+    setInterval(Animar,90);
 });
+
+function ConvertirTexto(){
+    resultado='';
+    cadena=$('#text-animation').html();
+    tamano=cadena.length;
+      for (var i = 0; i< tamano; i++) {
+          letra=cadena.charAt(i);
+          resultado+='<span class="ta-letra ">'+letra+'</span>';
+      }
+    
+    $('#text-animation').html(resultado);
+}
+
+function Animar(){
+    if(i<=tamano){
+        $('.ta-letra:nth-child('+i+')').css('color','#fff');
+    }
+    i++;
+}
